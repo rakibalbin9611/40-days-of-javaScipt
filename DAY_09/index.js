0491289edbbbb1d7fd9e5342d950800dd6947d87
rakibalbin9611 <rakibalbin17@gmail.com>
2025-07-05 16:49:46 +0600

    some changes

--- DAY_09/index.js
+++ DAY_09/index.js
@@ -24,21 +24,3 @@ console.log("Name is", name);
   //
   //
 }
-
-// 1. Expian Temporal Dead Zone by creating 3 variables in side a block. Post the code as your answer.
-{
-  // === TDZ for firstName, lastName starts here ===
-  // 'nickname' is declared with var — no TDZ for var
-
-  console.log(nickname); //  undefined (var is hoisted and initialized)
-  console.log(firstName); //  ReferenceError (TDZ)
-  console.log(lastName); //  ReferenceError (TDZ)
-
-  var nickname = "Tapper"; // var: no TDZ, hoisted as undefined, now assigned
-  let firstName = "Tapa"; // TDZ for firstName ends here
-  const lastName = "Script"; // TDZ for lastName ends here
-
-  console.log(nickname); // "Tapper"
-  console.log(firstName); // "Tapa"
-  console.log(lastName); // "Script"
-}
--- DAY_09/task.js
+++ DAY_09/task.js
@@ -1,3 +1,21 @@
+// 1. Expian Temporal Dead Zone by creating 3 variables in side a block. Post the code as your answer.
+{
+  // === TDZ for firstName, lastName starts here ===
+  // 'nickname' is declared with var — no TDZ for var
+
+  console.log(nickname); //  undefined (var is hoisted and initialized)
+  console.log(firstName); //  ReferenceError (TDZ)
+  console.log(lastName); //  ReferenceError (TDZ)
+
+  var nickname = "Tapper"; // var: no TDZ, hoisted as undefined, now assigned
+  let firstName = "Tapa"; // TDZ for firstName ends here
+  const lastName = "Script"; // TDZ for lastName ends here
+
+  console.log(nickname); // "Tapper"
+  console.log(firstName); // "Tapa"
+  console.log(lastName); // "Script"
+}
+
 // 2. Explain Variable and Function Hoisting with Example. Post the code as your answer.
 // === Hoisting Example ===
 
